53d2a3f2d49ddf511aa6884883397260e0562c28
hero0804 <145448392+hero0804@users.noreply.github.com>
2024-04-07 03:28:35 +0500

    I try
    sorry

--- html/js.fun/index.js
+++ html/js.fun/index.js
@@ -1,16 +1,7 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('menu_active');
-})
-
-$('.menu-button').on('click', function(e) {
-    e.preventDefault();
-    $('.slide').toggleClass('slide_active');
-   })
-
-   var message = $('textarea#message_box').val();
+var message = $('textarea#message_box').val();
    var button = document.getElementById('btn');
 
    button.addEventListener('click',function(){
        console.log(message)
    });
+
